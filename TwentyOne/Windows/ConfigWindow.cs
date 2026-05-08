8fcb362d8f37667ce1b4f08caa2f80a813aa7e54
Sidney Hays <me@sidneyhays.com>
2026-05-08 14:08:19 -0400

    chore: remove unused imports from ConfigWindow after narration split

--- TwentyOne/Windows/ConfigWindow.cs
+++ TwentyOne/Windows/ConfigWindow.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text.Json;
 using Dalamud.Bindings.ImGui;
-using Dalamud.Interface.ImGuiFileDialog;
 using Dalamud.Interface.Windowing;
 using TwentyOne.Game;
 
